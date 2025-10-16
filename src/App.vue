b39d5c2e29578f7d86077196a63ba73f3e857826
HARRIS KURNIADI SUMBOGO <harriskurniadi2@gmail.com>
2025-10-16 08:25:17 +0700

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <u-app>
+    <u-header>
+      <template #title> MyTask </template>
+      <template #right>
+        <u-color-mode-button></u-color-mode-button>
+        <u-button
+          to="https://github.com/kikazuru"
+          target="_blank"
+          icon="simple-icons:github"
+          aria-label="GitHub"
+          color="neutral"
+          variant="ghost"
+        ></u-button>
+      </template>
+    </u-header>
     <u-main>
       <router-view></router-view>
     </u-main>

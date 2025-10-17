6e43f0b85e65fd1a3bbc49be055447980034635c
HARRIS KURNIADI SUMBOGO <harriskurniadi2@gmail.com>
2025-10-17 08:29:41 +0700

    add layout for tasks

--- src/pages/projects/ProjectDetail.vue
+++ src/pages/projects/ProjectDetail.vue
@@ -1,6 +1,20 @@
 <template>
   <u-container>
-    <div>Project {{ id }}</div>
+    <u-page-hero :title="`Project ${id}`"></u-page-hero>
+
+    <div class="grid grid-cols-4 gap-5">
+      <u-link v-for="i in 10" :key="i">
+        <u-card>
+          <template #header>
+            <div>Task {{ i }}</div>
+          </template>
+          <template #default> Task Description </template>
+          <template #footer>
+            <u-badge label="On Going"></u-badge>
+          </template>
+        </u-card>
+      </u-link>
+    </div>
   </u-container>
 </template>
 

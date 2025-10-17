a42a0c2be3b09c7bc5f5ca81a719ec9dbdb26575
HARRIS KURNIADI SUMBOGO <harriskurniadi2@gmail.com>
2025-10-17 08:57:31 +0700

    change task to list

--- src/pages/projects/ProjectDetail.vue
+++ src/pages/projects/ProjectDetail.vue
@@ -2,26 +2,49 @@
   <u-container>
     <u-page-hero :title="`Project ${id}`"></u-page-hero>
 
-    <div class="grid grid-cols-4 gap-5">
-      <u-link v-for="i in 10" :key="i">
-        <u-card>
-          <template #header>
-            <div>Task {{ i }}</div>
-          </template>
-          <template #default> Task Description </template>
-          <template #footer>
-            <u-badge label="On Going"></u-badge>
-          </template>
-        </u-card>
-      </u-link>
+    <div class="flex flex-col gap-2">
+      <template v-for="(task, i) in tasks" :key="i">
+        <div class="flex justify-between border-1 rounded p-2">
+          <div class="flex gap-2 flex-1">
+            <u-checkbox
+              :ui="{
+                root: 'items-center',
+              }"
+              v-model="task.value.isCompleted"
+            ></u-checkbox>
+            <div
+              @click="task.value.isCompleted = !task.value.isCompleted"
+              :class="{ 'line-through': task.value.isCompleted }"
+              class="cursor-pointer flex-1"
+            >
+              {{ task.value.title }}
+            </div>
+          </div>
+
+          <div>
+            <u-badge v-if="i % 3 == 0" color="info" label="Upcoming"></u-badge>
+            <u-badge v-if="i % 3 == 1" color="success" label="On Going"></u-badge>
+            <u-badge v-if="i % 3 == 2" color="error" label="Overdue"></u-badge>
+          </div>
+        </div>
+      </template>
     </div>
   </u-container>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
 const { id } = defineProps<{
   id: string
 }>()
+
+const tasks = Array.from({ length: 10 }, (_, i) =>
+  ref({
+    title: `Task ${i}`,
+    isCompleted: false,
+  }),
+)
 </script>
 
 <style scoped></style>
